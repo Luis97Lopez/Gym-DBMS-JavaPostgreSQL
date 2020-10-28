4017a3b0a990142d6da490e5a09053225344a1b8
Luis97Lopez <lpzr97@gmail.com>
2020-10-28 17:57:07 -0600

    Se agregan 2 tablas en el SCRIPT

--- Gimnasio.sql
+++ Gimnasio.sql
@@ -11,4 +11,21 @@ CREATE DATABASE "Gimnasio"
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 	
-CREATE SCHEMA "gimnasio";
+CREATE SCHEMA gimnasio;
+
+CREATE TABLE gimnasio.Horario(
+	IdHorario BIGSERIAL NOT NULL,
+	HoraInicio TIME NOT NULL,
+	HoraFin TIME NOT NULL,
+	
+	CONSTRAINT PK_Horario PRIMARY KEY (IdHorario)
+);
+
+CREATE TABLE gimnasio.Articulo(
+	IdArticulo BIGSERIAL NOT NULL,
+	Nombre VARCHAR(40) NOT NULL,
+	Precio NUMERIC(2) NOT NULL,
+	Existencia BOOLEAN NOT NULL,
+	
+	CONSTRAINT PK_Articulo PRIMARY KEY (IdArticulo)
+);
