74736cf3f10c1895d8ae4901276bc5231de722b7
Luis97Lopez <lpzr97@gmail.com>
2020-10-28 17:46:02 -0600

    Se crea el archivo de SQL para Postgres

---
+++ Gimnasio.sql
@@ -0,0 +1,14 @@
+-- Database: Gimnasio
+
+-- DROP DATABASE "Gimnasio";
+
+CREATE DATABASE "Gimnasio"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Mexico.1252'
+    LC_CTYPE = 'Spanish_Mexico.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE SCHEMA "gimnasio";
